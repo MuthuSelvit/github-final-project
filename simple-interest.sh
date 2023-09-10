6ee220e716f73ef49b1f9f78d64b0f6aa36b89ee
MuthuSelvit <30895408+MuthuSelvit@users.noreply.github.com>
2023-09-10 17:04:20 +0530

    Updated simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author:  MuthuSelvit
    # Additional Authors:
-   # <your GitHub username>
+   #  MuthuSelvit
    # Input:
    # p, principal amount
    # t, time period in years
